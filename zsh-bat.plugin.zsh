05af58a841f4caa527422de8ceb558b54bf64771
George Andrei Ots <george.ots@outlook.com>
2025-04-01 16:17:18 +0200

    add bat and --help aliases

--- zsh-bat.plugin.zsh
+++ zsh-bat.plugin.zsh
@@ -15,9 +15,17 @@ fi
 # Save the original system `cat` under `rcat`
 alias rcat=$(which cat)
 
+default_theme="Visual Studio Dark+"
+
 # Add some useful aliases
-alias cat="${batname} -pp --theme=\"Visual Studio Dark+\""
-alias less="${batname} -p --theme=\"Visual Studio Dark+\""
+alias cat="${batname} -pp --theme=\"${default_theme}\""
+alias less="${batname} -p --theme=\"${default_theme}\""
 alias batlog="${batname} -pp -l log"
+
+alias bat="${batname} --theme=\"${default_theme}\""
+
+# Add alias for '--help' and '-h'
+alias -g -- -h='-h 2>&1 | bat --language=help --style=plain'
+alias -g -- --help='--help 2>&1 | bat --language=help --style=plain'
 
 export MANPAGER="sh -c 'col -bx | ${batname} -l man -p'"
